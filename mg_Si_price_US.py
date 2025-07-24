db36263055340b70597165086c3f743d4d6cf01e
rmahjabin <157666365+rmahjabin@users.noreply.github.com>
2025-07-24 16:06:42 -0500

    Update mg_Si_price_US.py

--- mg_Si_price_US.py
+++ mg_Si_price_US.py
@@ -7,7 +7,7 @@
 import pandas as pd
 
 def calculate_mg_si_prices(polysilicon_price_US):
-    df = pd.read_csv(polysilicon_price_US)
+    mg_si_dataframe_prices = pd.read_csv(polysilicon_price_US)
     
     mg_si_percentage = 0.20  # 20% of total cost
     scrap_rate = 0.20        # 20% scrap rate
@@ -17,9 +17,9 @@ def calculate_mg_si_prices(polysilicon_price_US):
         mg_si_base_price = effective_mg_si_cost * (1 - scrap_rate)
         return round (mg_si_base_price, 2)
         
-    df['mg_si_price'] = df['total_cost_per_kg'].apply(calculate_price)
+    mg_si_dataframe_prices['mg_si_price'] = mg_si_dataframe_prices['total_cost_per_kg'].apply(calculate_price)
     
-    return df
+    return mg_si_dataframe_prices
 
 
 # In[ ]:
